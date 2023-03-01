2bee1802ad5c33be13c8b16545a9ab6689a23b09
Azizstar1992 <126476257+Azizstar1992@users.noreply.github.com>
2023-03-01 14:34:20 +0000

    final version

--- Project1/src/project1/Main.java
+++ Project1/src/project1/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("What should be reversed");
 		Scanner sc = new Scanner(System.in);
-		System.out.println(ReverseString.reverse(sc.next()));
+		System.out.println(ReverseString.reverse(sc.nextLine()));
 			
 	
 	}
