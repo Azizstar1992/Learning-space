513cb5491ed086c811ed47342b8256473c8096b3
Azizstar1992 <126476257+Azizstar1992@users.noreply.github.com>
2023-03-01 14:32:07 +0000

    final solution

--- Project1/src/project1/Main.java
+++ Project1/src/project1/Main.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("What should be reversed");
 		Scanner sc = new Scanner(System.in);
 		System.out.println(ReverseString.reverse(sc.next()));
+			
+	
 	}
 
 }
